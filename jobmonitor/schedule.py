1d6c1e90737e9d058479a4e4629a0286798c0e10
Thijs Vogels <thijs.vogels@epfl.ch>
2018-11-11 17:05:21 +0100

    Add a field 'scheduled_date'

--- jobmonitor/schedule.py
+++ jobmonitor/schedule.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import datetime
 import os
 import subprocess
 import tempfile
@@ -44,6 +45,7 @@ def main():
             'job': job_spec['name'],
             'config': job_spec['config'],
             'initialization': specification['initialization'],
+            'scheduled_date': datetime.datetime.utcnow(),
             'status': 'scheduled',
         }
 
