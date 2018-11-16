bbd41609d3893bd8a356942cd8fd04c9b4964bde
Thijs Vogels <thijs.vogels@epfl.ch>
2018-11-16 14:48:09 +0100

    Make job deletion scriptable

--- jobmonitor/delete.py
+++ jobmonitor/delete.py
@@ -18,26 +18,30 @@ def main():
     args = parser.parse_args()
 
     for job_id in args.job_ids:
-        print('# Job', job_id)
-        kill_result = kill_job_in_kubernetes(job_id)
-        if kill_result:
-            print('- Killed job {} in Kubernetes'.format(kill_result))
+        delete_job(job_id)
 
-        influx.query("DELETE WHERE job_id='{}'".format(job_id))
-        print('- Deleted all traces of this job id in InfluxDB')
 
-        job = job_by_id(job_id)
-        if job:
-            if 'output_dir' in job:
-                output_dir = job['output_dir']
-                shutil.rmtree(os.path.join(os.getenv('JOBMONITOR_RESULTS_DIR'), job['output_dir']))
-                print('- Cleared output directory {}'.format(output_dir))
+def delete_job(job_id):
+    print('# Job', job_id)
+    kill_result = kill_job_in_kubernetes(job_id)
+    if kill_result:
+        print('- Killed job {} in Kubernetes'.format(kill_result))
 
-            delete_job_by_id(job_id)
-            print('- Deleted entry in MongoDB')
-            print('\n')
-        else:
-            print('- No MongoDB entry found')
+    influx.query("DELETE WHERE job_id='{}'".format(job_id))
+    print('- Deleted all traces of this job id in InfluxDB')
+
+    job = job_by_id(job_id)
+    if job:
+        if 'output_dir' in job:
+            output_dir = job['output_dir']
+            shutil.rmtree(os.path.join(os.getenv('JOBMONITOR_RESULTS_DIR'), job['output_dir']))
+            print('- Cleared output directory {}'.format(output_dir))
+
+        delete_job_by_id(job_id)
+        print('- Deleted entry in MongoDB')
+        print('\n')
+    else:
+        print('- No MongoDB entry found')
 
 if __name__ == '__main__':
     main()
