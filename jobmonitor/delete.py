4089797da40d94acf1e7a4567c54f1903b6a0337
Thijs Vogels <thijs.vogels@epfl.ch>
2018-12-21 15:15:18 +0100

    Batch kill, reusing delete script

--- jobmonitor/delete.py
+++ jobmonitor/delete.py
@@ -4,16 +4,59 @@ import os
 import shutil
 from argparse import ArgumentParser
 
-from jobmonitor.api import delete_job_by_id, job_by_id
-from jobmonitor.connections import influx, mongo
-from jobmonitor.kill import kill_job_in_kubernetes
+import kubernetes
+from bson.objectid import ObjectId
+
+from jobmonitor.api import delete_job_by_id, job_by_id, kubernetes_delete_job
+from jobmonitor.connections import KUBERNETES_NAMESPACE, influx, mongo
 
 
 """
 Delete all traces of a previously scheduled job (kubernetes, mongodb, influxdb, filesystem)
 """
 
-def main():
+def delete_job(job_id):
+    print('# Job', job_id)
+    kill_result = kill_job_in_kubernetes(job_id)
+
+    influx.query("DELETE WHERE job_id='{}'".format(job_id))
+    print('- Deleted all traces of this job id in InfluxDB')
+
+    job = job_by_id(job_id)
+    if job:
+        if 'output_dir' in job:
+            output_dir = job['output_dir']
+            shutil.rmtree(os.path.join(os.getenv('JOBMONITOR_RESULTS_DIR'), job['output_dir']))
+            print('- Cleared output directory {}'.format(output_dir))
+
+        delete_job_by_id(job_id)
+        print('- Deleted entry in MongoDB')
+        print('\n')
+    else:
+        print('- No MongoDB entry found')
+
+
+def kill_job_in_kubernetes(job_id):
+    kubernetes.config.load_kube_config()
+    client = kubernetes.client.BatchV1Api()
+    job_results = client.list_namespaced_job(KUBERNETES_NAMESPACE, label_selector="job_id="+job_id)
+    if not job_results.items:
+        return False
+
+    job = job_results.items[0]
+    name = job.metadata.name
+    kubernetes_delete_job(name)
+
+    # Set status to CANCELED in MongoDB if the job is still RUNNING
+    mongo.job.update({ '_id': ObjectId(job_id), 'status': 'RUNNING' }, { '$set': { 'status': 'CANCELED' } })
+
+    if name:
+        print('- Killed job {} in Kubernetes'.format(name))
+
+    return name
+
+
+def main(delete_fn=delete_job, action_name='delete'):
     parser = ArgumentParser()
     parser.add_argument('job_ids', nargs='*', help='IDs of the jobs to be removed.')
     parser.add_argument('--job')
@@ -35,7 +78,7 @@ def main():
         for job in mongo.job.find(query, {}):
             to_be_deleted.append(str(job['_id']))
 
-    print('Should I delete:')
+    print('Should I {}:'.format(action_name))
     for id in to_be_deleted:
         job = job_by_id(id)
         print("- {experiment} / {job} -- {status}".format(**job))
@@ -48,30 +91,8 @@ def main():
         print('Canceled ...')
     else:
         for job_id in to_be_deleted:
-            delete_job(job_id)
+            delete_fn(job_id)
 
-
-def delete_job(job_id):
-    print('# Job', job_id)
-    kill_result = kill_job_in_kubernetes(job_id)
-    if kill_result:
-        print('- Killed job {} in Kubernetes'.format(kill_result))
-
-    influx.query("DELETE WHERE job_id='{}'".format(job_id))
-    print('- Deleted all traces of this job id in InfluxDB')
-
-    job = job_by_id(job_id)
-    if job:
-        if 'output_dir' in job:
-            output_dir = job['output_dir']
-            shutil.rmtree(os.path.join(os.getenv('JOBMONITOR_RESULTS_DIR'), job['output_dir']))
-            print('- Cleared output directory {}'.format(output_dir))
-
-        delete_job_by_id(job_id)
-        print('- Deleted entry in MongoDB')
-        print('\n')
-    else:
-        print('- No MongoDB entry found')
 
 if __name__ == '__main__':
     main()
--- jobmonitor/kill.py
+++ jobmonitor/kill.py
@@ -1,13 +1,6 @@
 #!/usr/bin/env python3
 
-import os
-from argparse import ArgumentParser
-
-import kubernetes
-from bson.objectid import ObjectId
-
-from jobmonitor.connections import KUBERNETES_NAMESPACE, mongo
-from jobmonitor.api import kubernetes_delete_job
+import jobmonitor.delete
 
 
 """
@@ -15,31 +8,7 @@ Kill a job if it is running on kubernetes
 """
 
 def main():
-    parser = ArgumentParser()
-    parser.add_argument('job_ids', nargs='+', help='IDs of the jobs to be removed.')
-    args = parser.parse_args()
-
-    for job_id in args.job_ids:
-        if kill_job_in_kubernetes(job_id):
-            print('Killed job {}'.format(job_id))
-        else:
-            print('Job {} was not running'.format(job_id))
-
-
-def kill_job_in_kubernetes(job_id):
-    kubernetes.config.load_kube_config()
-    client = kubernetes.client.BatchV1Api()
-    job_results = client.list_namespaced_job(KUBERNETES_NAMESPACE, label_selector="job_id="+job_id)
-    if not job_results.items:
-        return False
-
-    job = job_results.items[0]
-    name = job.metadata.name
-    kubernetes_delete_job(name)
-
-    # Set status to CANCELED in MongoDB if the job is still RUNNING
-    mongo.job.update({ '_id': ObjectId(job_id), 'status': 'RUNNING' }, { '$set': { 'status': 'CANCELED' } })
-    return name
+    jobmonitor.delete.main(jobmonitor.delete.kill_job_in_kubernetes, action_name='kill')
 
 
 if __name__ == '__main__':
