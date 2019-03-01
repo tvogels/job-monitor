f99fa6dbf42b872ccb6ebe71878278a71a43ef91
Thijs Vogels <thijs.vogels@epfl.ch>
2019-03-01 11:41:56 +0100

    GraphQL & Frontend: remove single-user publish script

--- frontend/publish.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-yarn build
-
-docker build . -t vogels_frontend \
-&& docker tag vogels_frontend ic-registry.epfl.ch/mlo/vogels_frontend \
-&& docker push ic-registry.epfl.ch/mlo/vogels_frontend \
-&& kubectl delete -f ./kubernetes \
-&& kubectl create -f ./kubernetes
--- kubernetes-templates/frontend/publish-template.sh
+++ kubernetes-templates/frontend/publish-template.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# This file contains user-specific stuff and is generated automatically from the template
+# kubernetes-templates/frontend/publish-template.sh
+
 docker build ../../frontend -t {{user.name}}_frontend \
   --build-arg GRAPHQL_HOST={{user.name}}-graphql.mlo.k8s.iccluster.epfl.ch \
   --build-arg GRAPHQL_PORT={{ports.graphql}} \
