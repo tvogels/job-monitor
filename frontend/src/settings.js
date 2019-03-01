296f3066a0da456b724979c3f146136688f29244
Thijs Vogels <thijs.vogels@epfl.ch>
2019-03-01 11:42:30 +0100

    Build the frontend inside docker with multi-stage build

--- frontend/src/settings.js
+++ frontend/src/settings.js
@@ -1,1 +1,3 @@
-export const GRAPHQL = `http://${process.env.GRAPHQL_HOST}:${process.env.GRAPHQL_PORT}/graphql`;
+const host = process.env.REACT_APP_GRAPHQL_HOST;
+const port = process.env.REACT_APP_GRAPHQL_PORT;
+export const GRAPHQL = `http://${host}:${port}`;
