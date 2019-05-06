4d8ca8370d85b031bcff089004bafb03f3cc561d
Thijs Vogels <thijs.vogels@epfl.ch>
2019-05-06 09:49:33 +0200

    Format show.py

--- jobmonitor/show.py
+++ jobmonitor/show.py
@@ -13,10 +13,11 @@ from jobmonitor.api import job_by_id
 Show a job's DB entry in YAML format
 """
 
+
 def main():
     parser = ArgumentParser()
-    parser.add_argument('job_id', help='ID of the job')
-    parser.add_argument('-c', '--config', help='With config', action='store_true', default=False)
+    parser.add_argument("job_id", help="ID of the job")
+    parser.add_argument("-c", "--config", help="With config", action="store_true", default=False)
     args = parser.parse_args()
 
     job = job_by_id(args.job_id)
@@ -26,14 +27,14 @@ def main():
         sys.exit(1)
 
     if not args.config:
-        del job['config']
+        del job["config"]
 
-    job['id'] = str(job.pop('_id'))
+    job["id"] = str(job.pop("_id"))
 
-    del job['environment'] # too lazy to fix a bug
+    del job["environment"]  # too lazy to fix a bug
 
     print(yaml.safe_dump(job))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
