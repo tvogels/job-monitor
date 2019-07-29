77b3f3fd14119e884ec953db80356c4ce3d3e82c
Thijs Vogels <thijs.vogels@epfl.ch>
2019-07-29 10:41:08 +0200

    Add password authentication option to metadata mongodb

--- jobmonitor/connections.py
+++ jobmonitor/connections.py
@@ -4,21 +4,23 @@ from gridfs import GridFS
 from influxdb import InfluxDBClient
 from pymongo import MongoClient
 
-KUBERNETES_NAMESPACE = os.getenv('JOBMONITOR_KUBERNETES_NAMESPACE', default='mlo')
+KUBERNETES_NAMESPACE = os.getenv("JOBMONITOR_KUBERNETES_NAMESPACE", default="mlo")
 
 
-__all__ = ['mongo', 'influx', 'gridfs', 'KUBERNETES_NAMESPACE']
+__all__ = ["mongo", "influx", "gridfs", "KUBERNETES_NAMESPACE"]
 
 mongo_client = MongoClient(
-    host=os.getenv('JOBMONITOR_METADATA_HOST'),
-    port=int(os.getenv('JOBMONITOR_METADATA_PORT'))
+    host=os.getenv("JOBMONITOR_METADATA_HOST"),
+    port=int(os.getenv("JOBMONITOR_METADATA_PORT")),
+    username=os.getenv("JOBMONITOR_METADATA_USER"),
+    password=os.getenv("JOBMONITOR_METADATA_PASS"),
 )
-mongo = getattr(mongo_client, os.getenv('JOBMONITOR_METADATA_DB'))
+mongo = getattr(mongo_client, os.getenv("JOBMONITOR_METADATA_DB"))
 
 gridfs = GridFS(mongo)
 
 influx = InfluxDBClient(
-    host=os.getenv('JOBMONITOR_TIMESERIES_HOST'),
-    port=int(os.getenv('JOBMONITOR_TIMESERIES_PORT')),
-    database=os.getenv('JOBMONITOR_TIMESERIES_DB')
+    host=os.getenv("JOBMONITOR_TIMESERIES_HOST"),
+    port=int(os.getenv("JOBMONITOR_TIMESERIES_PORT")),
+    database=os.getenv("JOBMONITOR_TIMESERIES_DB"),
 )
