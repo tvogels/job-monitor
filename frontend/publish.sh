ba0f6348635286ac8347990f94d1b099c3c06f19
Thijs Vogels <thijs.vogels@epfl.ch>
2020-11-18 17:38:03 +0100

    configured web app properly

--- frontend/publish.sh
+++ frontend/publish.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+VERSION=1.2
+
 # This file contains user-specific stuff and is generated automatically from the template
 docker build . -t frontend \
   --build-arg GRAPHQL_HOST=vogels-graphql.mlo.k8s.iccluster.epfl.ch \
   --build-arg GRAPHQL_PORT=30004 \
-&& docker tag frontend ic-registry.epfl.ch/mlo/vogels_frontend \
-&& docker push ic-registry.epfl.ch/mlo/vogels_frontend
+&& docker tag frontend ic-registry.epfl.ch/mlo/vogels_frontend:$VERSION \
+&& docker push ic-registry.epfl.ch/mlo/vogels_frontend:$VERSION \
+&& docker tag frontend ic-registry.epfl.ch/mlo/vogels_frontend:latest \
+&& docker push ic-registry.epfl.ch/mlo/vogels_frontend:latest
